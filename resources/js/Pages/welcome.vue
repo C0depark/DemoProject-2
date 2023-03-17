ac2dcdd787ce0f75cfebeafa930805f76e101440
Justin Park <justinpark@Justins-iMac.local>
2023-03-17 16:56:34 -0400

    first commit

---
+++ resources/js/Pages/welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pb-8">{{ name }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "Hello World!!!"
+        }
+    }
+}
+</script>
